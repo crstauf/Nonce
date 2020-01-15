d8cc6f1c8dfbcd3c055954a55a725f776c7c25e7
Caleb Stauffer <develop+github@calebstauffer.com>
2020-01-15 01:03:26 -0500

    init

---
+++ nonce.php
@@ -0,0 +1,370 @@
+<?php
+/**
+ * Plugin name: Nonce
+ * Description: A drop-in helper for WordPress nonces.
+ * Author: Caleb Stauffer
+ * Author URI: develop.calebstauffer.com
+ * Version: 1.0
+ */
+
+if ( !defined( 'ABSPATH' ) || !function_exists( 'add_filter' ) ) {
+	header( 'Status: 403 Forbidden' );
+	header( 'HTTP/1.1 403 Forbidden' );
+	exit();
+}
+
+/**
+ * Class.
+ */
+class Nonce {
+
+	/**
+	 * @var self[] Registered Nonces.
+	 */
+	protected static $nonces = array();
+
+	/**
+	 * @var string Format for the nonce's action.
+	 */
+	protected $format = '';
+
+	/**
+	 * @var array Arguments for debugging.
+	 */
+	public $debug_args = array();
+
+
+	/*
+	 ######  ########    ###    ######## ####  ######
+	##    ##    ##      ## ##      ##     ##  ##    ##
+	##          ##     ##   ##     ##     ##  ##
+	 ######     ##    ##     ##    ##     ##  ##
+	      ##    ##    #########    ##     ##  ##
+	##    ##    ##    ##     ##    ##     ##  ##    ##
+	 ######     ##    ##     ##    ##    ####  ######
+	*/
+
+	/**
+	 * Register a Nonce.
+	 *
+	 * Creates a Nonce, and adds it to cache.
+	 *
+	 * @param string $key
+	 * @param string|int $format
+	 * @param array $debug_args
+	 */
+	static function register( string $key, $format = -1, array $debug_args = array() ) {
+		if ( isset( static::$nonces[$key] ) ) {
+			trigger_error( sprintf( 'Nonce with key <code>%s</code> is already registered.', esc_html( $key ) ) );
+			return;
+		}
+
+		static::$nonces[$key] = new static( $format, $debug_args );
+	}
+
+	/**
+	 * Get a registered Nonce.
+	 *
+	 * @param string $key
+	 * @return false|self
+	 */
+	static function get( string $key ) {
+		if ( !isset( static::$nonces[$key] ) ) {
+			trigger_error( sprintf( 'Nonce with key <code>%s</code> is not registered.', esc_html( $key ) ) );
+			return false;
+		}
+
+		return static::$nonces[$key];
+	}
+
+
+	/*
+	##     ##    ###     ######   ####  ######
+	###   ###   ## ##   ##    ##   ##  ##    ##
+	#### ####  ##   ##  ##         ##  ##
+	## ### ## ##     ## ##   ####  ##  ##
+	##     ## ######### ##    ##   ##  ##
+	##     ## ##     ## ##    ##   ##  ##    ##
+	##     ## ##     ##  ######   ####  ######
+	*/
+
+	/**
+	 * Construct.
+	 *
+	 * @var string|int $format Format of the nonce action, for use with sprintf().
+	 * @var array $debug_args Arguments to use for debugging.
+	 */
+	function __construct( $format = -1, array $debug_args = array() ) {
+		$this->format = $format;
+
+		if ( !empty( $debug_args ) )
+			$this->debug_args = $debug_args;
+	}
+
+	/**
+	 * To string.
+	 *
+	 * @uses $this->token()
+	 * @return string
+	 */
+	function __toString() {
+		return $this->token();
+	}
+
+	/**
+	 * Getter.
+	 *
+	 * @param string $key
+	 * @return string
+	 */
+	function __get( string $key ) {
+		return $this->$key;
+	}
+
+	/**
+	 * Invoked.
+	 *
+	 * @param array $args
+	 * @uses $this->token()
+	 * @return string
+	 */
+	function __invoke( ...$args ) {
+		return $this->token( ...$args );
+	}
+
+
+	/*
+	########  ######## ########  ##     ##  ######
+	##     ## ##       ##     ## ##     ## ##    ##
+	##     ## ##       ##     ## ##     ## ##
+	##     ## ######   ########  ##     ## ##   ####
+	##     ## ##       ##     ## ##     ## ##    ##
+	##     ## ##       ##     ## ##     ## ##    ##
+	########  ######## ########   #######   ######
+	*/
+
+	/**
+	 * Debug info.
+	 *
+	 * Manually determine value of verify_admin(), because
+	 * check_admin_referer() calls wp_nonce_ays() if nonce
+	 * is invalid.
+	 *
+	 * @see check_admin_referer()
+	 *
+	 * @uses admin_url()
+	 * @uses wp_get_referer()
+	 * @uses $this->action()
+	 * @uses wp_verify_nonce()
+	 * @uses $this->token()
+	 * @uses $this->field()
+	 * @uses $this->url()
+	 * @uses $this->verify()
+	 * @uses $this->verify_ajax()
+	 *
+	 * @return array
+	 */
+	function __debugInfo() {
+		$adminurl     = strtolower( admin_url() );
+		$referer      = strtolower( wp_get_referer() );
+		$verify_admin = isset( $_REQUEST[ $query_arg ] )
+			? wp_verify_nonce( $_REQUEST[ $query_arg ], $this->action( ...$this->debug_args ) )
+			: false;
+
+		return array(
+
+			# Properties.
+			'debug_args' => $this->debug_args,
+			'format'     => $this->format,
+
+			# Output functions.
+			'__toString()'             => $this->token(  ...$this->debug_args ),
+			'action( ...$debug_args )' => $this->action( ...$this->debug_args ),
+			'token(  ...$debug_args )' => $this->token(  ...$this->debug_args ),
+			'field(     $debug_args )' => $this->field(     $this->debug_args ),
+			'url(       $debug_args )' => $this->url(       $this->debug_args ),
+
+			# Verification functions.
+			'verify()'       => $this->verify( $this->token( ...$this->debug_args ), ...$this->debug_args ),
+			'verify_ajax()'  => $this->verify_ajax( $this->debug_args, '_wpnonce', false ),
+			'verify_admin()' => $verify_admin,
+
+		);
+	}
+
+	/**
+	 * Extra debug info.
+	 *
+	 * @see wp_create_nonce()
+	 * @see wp_nonce_tick()
+	 *
+	 * @uses wp_get_current_user()
+	 * @uses $this->action()
+	 * @uses wp_get_session_token()
+	 * @uses wp_nonce_tick()
+	 * @uses wp_hash()
+	 *
+	 * @return array
+	 */
+	function extra_debug_info() {
+		$uid = ( int ) wp_get_current_user()->ID;
+
+		if ( !$uid )
+			$uid = apply_filters( 'nonce_user_logged_out', $uid, $this->action( ...$debug_args ) );
+
+		$token = wp_get_session_token();
+		$tick  = wp_nonce_tick();
+		$hash  = wp_hash( $tick . '|' . $this->action( ...$this->debug_args ) . '|' . $uid . '|' . $token, 'nonce' );
+
+		return array(
+			'uid'                    => $uid,
+			'wp_get_session_token()' => $token,
+			'wp_nonce_tick()'        => $tick,
+			'nonce_life'             => apply_filters( 'nonce_life', DAY_IN_SECONDS ),
+			'wp_hash()'              => $hash,
+		);
+	}
+
+
+	/*
+	 #######  ##     ## ######## ########  ##     ## ########
+	##     ## ##     ##    ##    ##     ## ##     ##    ##
+	##     ## ##     ##    ##    ##     ## ##     ##    ##
+	##     ## ##     ##    ##    ########  ##     ##    ##
+	##     ## ##     ##    ##    ##        ##     ##    ##
+	##     ## ##     ##    ##    ##        ##     ##    ##
+	 #######   #######     ##    ##         #######     ##
+	*/
+
+	/**
+	 * Nonce action.
+	 *
+	 * @param array $args
+	 * @return string
+	 */
+	function action( ...$args ) {
+		return !empty( $args )
+			? vsprintf( $this->format, $args )
+			: $this->format;
+	}
+
+	/**
+	 * Nonce token.
+	 *
+	 * @param array $args
+	 * @uses $this->action()
+	 * @uses wp_create_nonce()
+	 * @return string
+	 */
+	function token( ...$args ) {
+		return wp_create_nonce( $this->action( ...$args ) );
+	}
+
+	/**
+	 * Nonce field.
+	 *
+	 * @param array $args
+	 * @param string $name
+	 * @param bool $referrer
+	 *
+	 * @uses $this->action()
+	 * @uses wp_nonce_field()
+	 *
+	 * @return string
+	 */
+	function field( array $args = array(), string $name = '_wpnonce', bool $referrer = true ) {
+		return wp_nonce_field( $this->action( ...$args ), $name, $referrer, false );
+	}
+
+	/**
+	 * Add nonce to URL.
+	 *
+	 * @param array $args
+	 * @param string $name
+	 * @param false|string $url
+	 *
+	 * @uses $this->action()
+	 * @uses add_query_arg()
+	 *
+	 * @return string
+	 */
+	function url( array $args = array(), string $name = '_wpnonce', $url = false ) {
+		return add_query_arg( $name, $this->token( ...$args ), $url );
+	}
+
+
+	/*
+	##     ## ######## ########  #### ######## ##    ##
+	##     ## ##       ##     ##  ##  ##        ##  ##
+	##     ## ##       ##     ##  ##  ##         ####
+	##     ## ######   ########   ##  ######      ##
+	 ##   ##  ##       ##   ##    ##  ##          ##
+	  ## ##   ##       ##    ##   ##  ##          ##
+	   ###    ######## ##     ## #### ##          ##
+	*/
+
+	/**
+	 * Verify.
+	 *
+	 * @param string $value
+	 * @param array $args
+	 *
+	 * @uses $this->action()
+	 * @uses wp_verify_nonce()
+	 *
+	 * @return false|int
+	 */
+	function verify( string $value, ...$args ) {
+		return wp_verify_nonce( $value, $this->action( ...$args ) );
+	}
+
+	/**
+	 * Verify AJAX referer.
+	 *
+	 * @param array $args
+	 * @param false|string $name
+	 * @param bool $die
+	 *
+	 * @uses $this->action()
+	 * @uses check_ajax_referer()
+	 *
+	 * @return false|int
+	 */
+	function verify_ajax( array $args = array(), $name = false, bool $die = true ) {
+		return check_ajax_referer( $this->action( ...$args ), $name, $die );
+	}
+
+	/**
+	 * Verify admin referer.
+	 *
+	 * @param array $args
+	 * @param string $name
+	 *
+	 * @uses $this->action()
+	 * @uses check_admin_referer()
+	 *
+	 * @return false|int
+	 */
+	function verify_admin( array $args = array(), string $name = '_wpnonce' ) {
+		return check_admin_referer( $this->action( ...$args ), $name );
+	}
+
+}
+
+add_action( 'init', function() {
+	$nonce = new Nonce( 'qwerty %s %d' );
+	$id = 'caleb';
+	$rand = 15;
+	$nonce->debug_args = array( $id, $rand );
+	echo $nonce( $id, $rand );
+	var_dump( $nonce );
+
+	$action = $nonce->action( $id, $rand );
+	$token = $nonce->token( $id, $rand );
+	$verify = $nonce->verify( $token, $id, $rand );
+
+	echo $action . "\n" . $token . "\n" . ( $verify ? 'verified' : 'unverified' );
+	exit;
+} );
+
+?>
---
+++ readme.md
@@ -0,0 +1,173 @@
+# _Nonce_
+
+A helper for creating WordPress nonces.
+
+## Documentation
+
+### Static and Magic Functions
+
+#### `Nonce::register()`
+
+Registering a _Nonce_ stores the object, making it accessible later using `Nonce::get()` via a specified key.
+
+```php
+Nonce::register( 'example', 'The Nonce example action' );
+```
+
+#### `Nonce::get()`
+
+Gets registered _Nonce_ at the specified key.
+
+```php
+<?php
+$nonce = Nonce::get( 'example' );
+```
+
+#### `new Nonce()`
+
+_Nonces_ do not have to be registered: they can be created on the fly, and stored elsewhere.
+
+|Parameter|Description|
+|:---|:---|
+|`$format`|Format of the nonce's action, for use within `vsprintf()`. Default: `-1`.|
+|`$debug_args`|Array of values for the `vsprintf()` when `__debugInfo()` function is called.|
+
+```php
+<?php
+$nonce = new Nonce( 'update Post %s (%d)', array( 'post_name', rand( 1, 100 ) ) );
+```
+
+#### `echo Nonce`
+
+Printing a _Nonce_ object will print the nonce, without replacing the conversion specifications with values.
+
+```php
+<?php
+$nonce = new Nonce( 'update Post %s (%d)' );
+echo $nonce;
+// equivalent to wp_create_nonce( 'update Post %s (%d)' )
+```
+
+#### `Nonce()`
+
+You can invoke the _Nonce_ object to get the nonce's token, using the passed arguments.
+
+```php
+<?php
+$nonce = new Nonce( 'update Post %s (%d)' );
+$token = $nonce( 'post_name', 392 );
+// $token === $nonce->token( 'post_name', 392 )
+// $token === wp_create_nonce( 'update Post post_name (392)' )
+```
+
+### Output Functions
+
+#### `Nonce->action()`
+
+Gets the action of the nonce.
+
+```php
+<?php
+$nonce = new Nonce( 'update Post %s (%d)' );
+echo $nonce->action( 'post_name', 392 );
+// "update Post post_name (392)"
+```
+
+#### `Nonce->token()`
+
+Gets the nonce (token).
+
+```php
+<?php
+$nonce = new Nonce( 'update Post %s (%d)' );
+$token = $nonce->token( 'post_name', 392 );
+// $token === wp_create_nonce( 'update Post post_name (392)' )
+```
+
+#### `Nonce->field()`
+
+Get the HTML for the nonce form fields.
+
+|Parameter|Description|
+|:---|:---|
+|`$args`|Array of values for the conversion specifications in the `format` property, to generate the action and token.|
+|`$name`|Form field's name. Default: `_wpnonce`.|
+|`$referrer`|Bool to include the referrer field. Default: `true`.|
+
+```php
+<?php
+$nonce = new Nonce( 'update Post %s (%d)' );
+echo $nonce->field( array( 'post_name', 392 ), '_wpnonce', false );
+// <input type="hidden" id="_wpnonce" name="_wpnonce" value="{NONCE}}" />
+```
+
+#### `Nonce->url()`
+
+Add the nonce onto a URL.
+
+|Parameter|Description|
+|:---|:---|
+|`$args`|Array of values for the conversion specifications in the `format` property, to generate the action and token.|
+|`$name`|Query argument name. Default: `_wpnonce`.|
+|`$url`|URL to add the nonce onto. Default: `false` (uses the current URL).|
+
+```php
+<?php
+$nonce = new Nonce( 'update Post %s (%d)' );
+echo $nonce->url( array( 'post_name', 392 ), '_wpnonce', 'https://google.com' );
+// https://google.com?_wpnonce={NONCE}
+```
+
+### Verification Functions
+
+#### `Nonce->verify()`
+
+Verify the provided value matches the nonce's action.
+
+|Parameter|Description|
+|:---|:---|
+|`$value`|Value to compare to the token.|
+|`$args`|Array of values for the conversion specifications in the `format` property, to generate the action.|
+
+```php
+<?php
+$nonce = new Nonce( 'update Post %s (%d)' );
+$verified = $nonce->verify( $_POST['_wpnonce'], 'post_name', 392 );
+// $verified === true if $_POST['_wpnonce'] value matches nonce
+```
+
+#### `Nonce->verify_ajax()`
+
+Verify nonce in AJAX request.
+
+|Parameter|Description|
+|:---|:---|
+|`$args`|Array of values for the conversion specifications in the `format` property, to generate the action.|
+|`$name`|Name of key to check for nonce in `$_REQUEST`. Default: `false`.|
+|`$die`|Bool to terminate script execution if nonce is invalid.|
+
+```php
+<?php
+$nonce = new Nonce( 'update Post %s (%d)' );
+$verified = $nonce->verify_ajax( array( 'post_name', 392 ), false, false );
+// $verified === true if value of $_REQUEST['_ajax_nonce'] or $_REQUEST['_wpnonce'] matches nonce
+```
+
+#### `Nonce->verify_admin()`
+
+Verify nonce is valid within admin context.
+
+|Parameter|Description|
+|:---|:---|
+|`$args`|Array of values for the conversion specifications in the `format` property, to generate the action.|
+|`$name`|Name of key to check for nonce in `$_REQUEST`. Default: `_wpnonce`.|
+
+### Debug Functions
+
+#### `Nonce::__debugInfo()`
+
+Returns an array of properties and functions using debug data provided in the `debug_args` property.
+
+#### `Nonce->extra_debug_info()`
+
+Returns an array of values that `wp_create_nonce()` uses to generate the nonce.
