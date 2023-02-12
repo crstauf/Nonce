2f0e7c76fda58ad22d5f3b3d95bb0ea43bf96808
Caleb Stauffer <develop+github@calebstauffer.com>
2023-02-11 22:37:59 -0500

    cleanup

--- nonce.php
+++ nonce.php
@@ -7,7 +7,7 @@
  * Version: 1.0
  */
 
-if ( !defined( 'ABSPATH' ) || !function_exists( 'add_filter' ) ) {
+if ( ! defined( 'WPINC' ) || ! function_exists( 'add_filter' ) ) {
 	header( 'Status: 403 Forbidden' );
 	header( 'HTTP/1.1 403 Forbidden' );
 	exit();
@@ -29,7 +29,7 @@ class Nonce {
 	protected $format = '';
 
 	/**
-	 * @var array Arguments for debugging.
+	 * @var mixed[] Arguments for debugging.
 	 */
 	public $args = array();
 
@@ -51,30 +51,32 @@ class Nonce {
 	 *
 	 * @param string $key
 	 * @param string|int $format
-	 * @param array $args
+	 * @param mixed[] $args
+	 *
+	 * @return void
 	 */
-	static function register( string $key, $format = -1, array $args = array() ) {
-		if ( isset( static::$nonces[$key] ) ) {
-			trigger_error( sprintf( 'Nonce with key <code>%s</code> is already registered.', esc_html( $key ) ) );
+	public static function register( string $key, $format = -1, array $args = array() ) : void {
+		if ( isset( static::$nonces[ $key ] ) ) {
+			trigger_error( sprintf( 'Nonce with key <code>%s</code> is already registered.', $key ) );
 			return;
 		}
 
-		static::$nonces[$key] = new static( $format, $args );
+		static::$nonces[ $key ] = new static( $format, $args );
 	}
 
 	/**
 	 * Get a registered Nonce.
 	 *
 	 * @param string $key
-	 * @return false|self
+	 * @return self
 	 */
-	static function get( string $key ) {
-		if ( !isset( static::$nonces[$key] ) ) {
+	public static function get( string $key ) : static {
+		if ( ! isset( static::$nonces[ $key ] ) ) {
 			trigger_error( sprintf( 'Nonce with key <code>%s</code> is not registered.', esc_html( $key ) ) );
-			return false;
+			return new static;
 		}
 
-		return static::$nonces[$key];
+		return static::$nonces[ $key ];
 	}
 
 
@@ -92,13 +94,16 @@ class Nonce {
 	 * Construct.
 	 *
 	 * @var string|int $format Format of the nonce action, for use with sprintf().
-	 * @var array $args Arguments to use for debugging.
+	 * @var mixed[] $args Arguments to use for debugging.
 	 */
-	function __construct( $format = -1, array $args = array() ) {
+	public function __construct( $format = -1, array $args = array() ) {
 		$this->format = $format;
 
-		if ( !empty( $args ) )
-			$this->args = $args;
+		if ( empty( $args ) ) {
+			return;
+		}
+		
+		$this->args = $args;
 	}
 
 	/**
@@ -107,7 +112,7 @@ class Nonce {
 	 * @uses $this->token()
 	 * @return string
 	 */
-	function __toString() {
+	public function __toString() : string {
 		return $this->token();
 	}
 
@@ -115,20 +120,20 @@ class Nonce {
 	 * Getter.
 	 *
 	 * @param string $key
-	 * @return string
+	 * @return mixed
 	 */
-	function __get( string $key ) {
+	public function __get( string $key ) {
 		return $this->$key;
 	}
 
 	/**
 	 * Invoked.
 	 *
-	 * @param array $args
+	 * @param mixed[] $args
 	 * @uses $this->token()
 	 * @return string
 	 */
-	function __invoke( ...$args ) {
+	public function __invoke( ...$args ) : string {
 		return $this->token( ...$args );
 	}
 
@@ -162,9 +167,9 @@ class Nonce {
 	 * @uses $this->verify()
 	 * @uses $this->verify_ajax()
 	 *
-	 * @return array
+	 * @return mixed[]
 	 */
-	function __debugInfo() {
+	public function __debugInfo() : array {
 		$adminurl     = strtolower( admin_url() );
 		$referer      = strtolower( wp_get_referer() );
 		$verify_admin = isset( $_REQUEST[ $query_arg ] )
@@ -174,20 +179,20 @@ class Nonce {
 		return array(
 
 			# Properties.
-			'args' => $this->args,
-			'format'     => $this->format,
+			'args'               => $this->args,
+			'format'             => $this->format,
 
 			# Output functions.
-			'__toString()'             => $this->token(  ...$this->args ),
+			'__toString()'       => $this->token(  ...$this->args ),
 			'action( ...$args )' => $this->action( ...$this->args ),
 			'token(  ...$args )' => $this->token(  ...$this->args ),
 			'field(     $args )' => $this->field(     $this->args ),
 			'url(       $args )' => $this->url(       $this->args ),
 
 			# Verification functions.
-			'verify()'       => $this->verify( $this->token( ...$this->args ), ...$this->args ),
-			'verify_ajax()'  => $this->verify_ajax( $this->args, '_wpnonce', false ),
-			'verify_admin()' => $verify_admin,
+			'verify()'           => $this->verify( $this->token( ...$this->args ), ...$this->args ),
+			'verify_ajax()'      => $this->verify_ajax( $this->args, '_wpnonce', false ),
+			'verify_admin()'     => $verify_admin,
 
 		);
 	}
@@ -204,17 +209,19 @@ class Nonce {
 	 * @uses wp_nonce_tick()
 	 * @uses wp_hash()
 	 *
-	 * @return array
+	 * @return mixed[]
 	 */
-	function extra_debug_info() {
-		$uid = ( int ) wp_get_current_user()->ID;
+	public function extra_debug_info() : array {
+		$uid    = ( int ) wp_get_current_user()->ID;
+		$action = $this->action( ...$this->args );
 
-		if ( !$uid )
-			$uid = apply_filters( 'nonce_user_logged_out', $uid, $this->action( ...$args ) );
+		if ( empty( $uid ) ) {
+			$uid = apply_filters( 'nonce_user_logged_out', $uid, $action );
+		}
 
 		$token = wp_get_session_token();
 		$tick  = wp_nonce_tick();
-		$hash  = wp_hash( $tick . '|' . $this->action( ...$this->args ) . '|' . $uid . '|' . $token, 'nonce' );
+		$hash  = wp_hash( $tick . '|' . $action . '|' . $uid . '|' . $token, 'nonce' );
 
 		return array(
 			'uid'                    => $uid,
@@ -239,31 +246,33 @@ class Nonce {
 	/**
 	 * Nonce action.
 	 *
-	 * @param array $args
+	 * @param mixed[] $args
 	 * @return string
 	 */
-	function action( ...$args ) {
-		return !empty( $args )
-			? vsprintf( $this->format, $args )
-			: $this->format;
+	public function action( ...$args ) : string {
+		if ( empty( $args ) ) {
+			return $this->format;
+		}
+		
+		return vsprintf( $this->format, $args );
 	}
 
 	/**
 	 * Nonce token.
 	 *
-	 * @param array $args
+	 * @param mixed[] $args
 	 * @uses $this->action()
 	 * @uses wp_create_nonce()
 	 * @return string
 	 */
-	function token( ...$args ) {
+	public function token( ...$args ) : string {
 		return wp_create_nonce( $this->action( ...$args ) );
 	}
 
 	/**
 	 * Nonce field.
 	 *
-	 * @param array $args
+	 * @param mixed[] $args
 	 * @param string $name
 	 * @param bool $referrer
 	 *
@@ -272,14 +281,14 @@ class Nonce {
 	 *
 	 * @return string
 	 */
-	function field( array $args = array(), string $name = '_wpnonce', bool $referrer = true ) {
+	public function field( array $args = array(), string $name = '_wpnonce', bool $referrer = true ) : string {
 		return wp_nonce_field( $this->action( ...$args ), $name, $referrer, false );
 	}
 
 	/**
 	 * Add nonce to URL.
 	 *
-	 * @param array $args
+	 * @param mixed[] $args
 	 * @param string $name
 	 * @param false|string $url
 	 *
@@ -288,7 +297,7 @@ class Nonce {
 	 *
 	 * @return string
 	 */
-	function url( array $args = array(), string $name = '_wpnonce', $url = false ) {
+	public function url( array $args = array(), string $name = '_wpnonce', $url = false ) : string {
 		return add_query_arg( $name, $this->token( ...$args ), $url );
 	}
 
@@ -307,64 +316,62 @@ class Nonce {
 	 * Verify.
 	 *
 	 * @param string $value
-	 * @param array $args
+	 * @param mixed[] $args
 	 *
 	 * @uses $this->action()
 	 * @uses wp_verify_nonce()
 	 *
-	 * @return false|int
+	 * @return int
 	 */
-	function verify( string $value, ...$args ) {
-		return wp_verify_nonce( $value, $this->action( ...$args ) );
+	public function verify( string $value, ...$args ) : int {
+		return ( int ) wp_verify_nonce( $value, $this->action( ...$args ) );
 	}
 
 	/**
 	 * Verify AJAX referer.
 	 *
-	 * @param array $args
+	 * @param mixed[] $args
 	 * @param false|string $name
 	 * @param bool $die
 	 *
 	 * @uses $this->action()
 	 * @uses check_ajax_referer()
 	 *
-	 * @return false|int
+	 * @return int
 	 */
-	function verify_ajax( array $args = array(), $name = false, bool $die = true ) {
-		return check_ajax_referer( $this->action( ...$args ), $name, $die );
+	public function verify_ajax( array $args = array(), $name = false, bool $die = true ) : int {
+		return ( int ) check_ajax_referer( $this->action( ...$args ), $name, $die );
 	}
 
 	/**
 	 * Verify admin referer.
 	 *
-	 * @param array $args
+	 * @param mixed[] $args
 	 * @param string $name
 	 *
 	 * @uses $this->action()
 	 * @uses check_admin_referer()
 	 *
-	 * @return false|int
+	 * @return int
 	 */
-	function verify_admin( array $args = array(), string $name = '_wpnonce' ) {
-		return check_admin_referer( $this->action( ...$args ), $name );
+	public function verify_admin( array $args = array(), string $name = '_wpnonce' ) : int {
+		return ( int ) check_admin_referer( $this->action( ...$args ), $name );
 	}
 
 }
 
 add_action( 'init', function() {
-	$nonce = new Nonce( 'qwerty %s %d' );
-	$id = 'caleb';
-	$rand = 15;
-	$nonce->args = array( $id, $rand );
+	$id    = 'caleb';
+	$rand  = 15;
+	$nonce = new Nonce( 'qwerty %s %d', array( $id, $rand ) );
+
 	echo $nonce( $id, $rand );
 	var_dump( $nonce );
 
 	$action = $nonce->action( $id, $rand );
-	$token = $nonce->token( $id, $rand );
+	$token  = $nonce->token(  $id, $rand );
 	$verify = $nonce->verify( $token, $id, $rand );
 
 	echo $action . "\n" . $token . "\n" . ( $verify ? 'verified' : 'unverified' );
 	exit;
 } );
-
-?>
